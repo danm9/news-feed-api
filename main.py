e1ba9c7d804b17c86c9b546cbf9358f2bab57cf1
dan <dan@localhost.localdomain>
2021-10-30 19:25:15 -0400

    added in CORS

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 import GNewsAPI
 import newsapiAPI
 
 app = Flask(__name__)
-# api = Api(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def index():
     return '<h1>This API is for Dan to get news from multiple resources</h1>'
 
 @app.route('/gnews')
+@cross_origin()
 def get_gnews_testing():
     return {'news': GNewsAPI.gnews_testing_data}
 
 @app.route('/news_api_testing')
+@cross_origin()
 def get_news_api_testing():
     return {'news': newsapiAPI.news_api_testing_data}
 
 @app.route('/news_api_headlines')
+@cross_origin()
 def get_news_api_headlines():
     return {'news': newsapiAPI.news_api_headlines_data}
 
