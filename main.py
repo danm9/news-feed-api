209c42a87dfe6e606eb1612c43b1c9ccc2135196
dan <dan@localhost.localdomain>
2021-10-30 19:38:44 -0400

    again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import GNewsAPI
 import newsapiAPI
 
@@ -11,14 +11,17 @@ def index():
     return '<h1>This API is for Dan to get news from multiple resources</h1>'
 
 @app.route('/gnews')
+@cross_origin()
 def get_gnews_testing():
     return {'news': GNewsAPI.gnews_testing_data}
 
 @app.route('/news_api_testing')
+@cross_origin()
 def get_news_api_testing():
     return {'news': newsapiAPI.news_api_testing_data}
 
 @app.route('/news_api_headlines')
+@cross_origin()
 def get_news_api_headlines():
     return {'news': newsapiAPI.news_api_headlines_data}
 
