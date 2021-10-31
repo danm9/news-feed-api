0b89b9763c0c58757919863011c205c2c60bbe7b
dan <dan@localhost.localdomain>
2021-10-30 21:20:44 -0400

    removed gnews

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
-import GNewsAPI
 import newsapiAPI
 
 app = Flask(__name__)
@@ -9,11 +8,6 @@ CORS(app)
 @app.route('/')
 def index():
     return '<h1>This API is for Dan to get news from multiple resources</h1>'
-
-@app.route('/gnews')
-@cross_origin()
-def get_gnews_testing():
-    return {'news': GNewsAPI.gnews_testing_data}
 
 @app.route('/news_api_testing')
 @cross_origin()
