d6d357b877b443a38a3eb350b9829da523012e6a
dan <dan@localhost.localdomain>
2021-11-06 23:13:27 -0400

    updated api info

--- newsapiAPI.py
+++ newsapiAPI.py
@@ -5,23 +5,22 @@ url = 'https://newsapi.org/v2/'
 api_key = 'apiKey=258d0dcae7d14d18baae22d847567ee2'
 
 # everything with sources and keywords
-t = 'everything'
+slug = 'everything'
 sources = 'sources=techcrunch, business-insider, fortune, next-big-future, hacker-news, recode, reuters, techradar, the-next-web, wired,'
 q = 'q="quality assurance" OR "software testing" OR "software development" OR "quality testing" OR "automated testing"'
 page_size = 'pageSize=100'
 date = str(date.today() - timedelta(7))
 f = 'from='+date
 
-news_api_testing_api = url + t + '?' + sources + '&' + q + '&' + page_size + '&' + f + '&' + api_key
-
-print(news_api_testing_api)
-
-
-# news_api_testing_api = 'https://newsapi.org/v2/everything?sources=techcrunch, business-insider, fortune, next-big-future, hacker-news, recode, reuters, techradar, the-next-web, wired,&q="quality assurance" OR "software testing" OR "software development" OR "quality testing" OR "automated testing"&apiKey=258d0dcae7d14d18baae22d847567ee2&pageSize=100'
+news_api_testing_api = '{}{}?{}&{}&{}&{}&{}'.format(url, slug, sources, q, page_size, f, api_key)
 response = requests.get(news_api_testing_api)
 news_api_testing_data = response.json()
 
 # headlines for technology
-news_api_headlines_api = 'https://newsapi.org/v2/top-headlines?q=testing&apiKey=258d0dcae7d14d18baae22d847567ee2&category=technology'
+slug = 'top-headlines'
+q = 'q="testing" OR "software"'
+category = 'category=technology'
+
+news_api_headlines_api = '{}{}?{}&{}'.format(url, slug, q, api_key)
 response = requests.get(news_api_headlines_api)
 news_api_headlines_data = response.json()
